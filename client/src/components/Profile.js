cb8355714b2000c97eae60abe4ffd890dd08abb1
Joe Friel <joseph.s.friel@gmail.com>
2018-10-05 11:58:06 +0100

    Add dynamic data for profile basic info > rel issue #16

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -4,21 +4,54 @@ import { Link } from "react-router-dom";
 import Header from "./ProfileComponents/Header";
 import Contact from "./ProfileComponents/Contact";
 import HomeBtn from "./HomeBtn";
+import dummyProfile from "../utils/dummyProfileData";
 
 export default class Profile extends React.Component {
+  state = {
+    response: null,
+    loading: true
+  };
+
+  componentDidMount() {
+    this.populateProfile();
+  }
+
+  populateProfile = () => {
+    this.setState({
+      response: dummyProfile,
+      loading: false
+    });
+    console.log("response", this.state.response);
+  };
+
   render() {
+    console.log("response", this.state.response);
+
+    // destructure the state
+
+    if (this.state.loading) {
+      return <h1>Loading...</h1>;
+    }
+
+    const {
+      basic_info: basicInfo,
+      product,
+      impact,
+      ˀimages_demos: assets
+    } = this.state.response;
+
     return (
       <React.Fragment>
         <HomeBtn />
         <Header
-          compName="Company Name"
-          website="www.website.com"
-          desc="A short descriptor"
+          compName={basicInfo.company_name}
+          website={basicInfo.website}
+          desc={basicInfo.one_liner}
         />
         <Contact
-          name="Jessie Beech"
-          title="Head of Fun"
-          email="jessie@cool.com"
+          name={basicInfo.contact_name}
+          title={basicInfo.contact_title}
+          email={basicInfo.email}
         />
         <Link to="/profile/id/add">
           <button>Add more content</button>
---
+++ client/src/utils/dummyProfileData.js
@@ -0,0 +1,33 @@
+const dummyProfile = {
+  basic_info: {
+    company_name: "Senzing",
+    email: "senzing@senzing.com",
+    one_liner:
+      "Super duper fancy technological solution that in some way is relevant for local government but we don't know how or why",
+    contact_name: "Jessie Beech",
+    contact_title: "Head of Fun",
+    website: "http://www.senzing.com",
+    logo_url: "logo.jpg"
+  },
+  product: {
+    why_useful: {
+      descrip:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting ˀindustry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+      helper:
+        "Its important to use plain english and avoid any technical jargon!",
+      input_type: "short_text"
+    }
+  },
+  impact: {
+    roi_spreadsheet: {
+      file_url: "file.xlsx",
+      input_type: "file_upload"
+    }
+  },
+  images_demos: {
+    video_demo_url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+    img_urls: ["image1.jpg", "image2.jpg"]
+  }
+};
+
+export default dummyProfile;
