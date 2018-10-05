1695cfea8713c20121897cef54a07ca928c8a46d
Joe Friel <joseph.s.friel@gmail.com>
2018-10-05 14:53:44 +0100

    Build proper dummy profile data object > rel issue #6

--- src/getProfileData.js
+++ src/getProfileData.js
@@ -13,23 +13,44 @@ const profileData = {
     website: "http://www.senzing.com",
     logo_url: "logo.jpg"
   },
-  product: {
-    why_useful: {
-      descrip:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting ˀindustry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-      helper:
-        "Its important to use plain english and avoid any technical jargon!",
-      input_type: "short_text"
+  answers: [
+    {
+      question: "Why is this product useful to Local Authorities?",
+      answer:
+        "Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris. Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus resi dentevil vultus comedat cerebella viventium. Qui animated corpse, cricket bat max brucks terribilem incessu zomby. The voodoo sacerdos flesh eater, suscitat mortuos comedere carnem virus. Zonbi tattered for solum oculi eorum defunctis go lum cerebro.",
+      category: "product",
+      input_type: "short text",
+      helper_text:
+        "Its important to use plain english and avoid any technical jargon!"
+    },
+    {
+      question: "What type of council(s) is it running with?",
+      answer: "District",
+      category: "Local Authorities",
+      input_type: "dropdown",
+      helper_text: ""
+    },
+    {
+      question: "Examples of council implementation. What were the outcomes?",
+      answer: `Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris. Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus resi dentevil vultus comedat cerebella viventium. Qui animated corpse, cricket bat max brucks terribilem incessu zomby. The voodoo sacerdos flesh eater, suscitat mortuos comedere carnem virus. Zonbi tattered for solum oculi eorum defunctis go lum cerebro. Nescio brains an Undead zombies. Sicut malus putrid voodoo horror. Nigh tofth eliv ingdead.
+
+    Cum horribilem walking dead resurgere de crazed sepulcris creaturis, zombie sicut de grave feeding iride et serpens. Pestilentia, shaun ofthe dead scythe animated corpses ipsa screams. Pestilentia est plague haec decaying ambulabat mortuos. Sicut zeder apathetic malus voodoo. Aenean a dolor plan et terror soulless vulnerum contagium accedunt, mortui iam vivam unlife. Qui tardius moveri, brid eof reanimator sed in magna copia sint terribiles undeath legionis. Alii missing oculis aliorum sicut serpere crabs nostram. Putridi braindead odores kill and infect, aere implent left four dead.
+    
+    Lucio fulci tremor est dark vivos magna. Expansis creepy arm yof darkness ulnis witchcraft missing carnem armis Kirkman Moore and Adlard caeruleum in locis. Romero morbo Congress amarus in auras. Nihil horum sagittis tincidunt, zombie slack-jawed gelida survival portenta. The unleashed virus est, et iam zombie mortui ambulabunt super terram. Souless mortuum glassy-eyed oculos attonitos indifferent back zom bieapoc alypse. An hoc dead snow braaaiiiins sociopathic incipere Clairvius Narcisse, an ante? Is bello mundi z?
+    
+    In Craven omni memoria patriae zombieland clairvius narcisse religionis sunt diri undead historiarum. Golums, zombies unrelenting et Raimi fascinati beheading. Maleficia! Vel cemetery man a modern bursting eyeballs perhsaps morbi. A terrenti flesh contagium. Forsitan deadgurl illud corpse Apocalypsi, vel staggering malum zomby poenae chainsaw zombi horrifying fecimus burial ground. Indeflexus shotgun coup de poudre monstra per plateas currere. Fit de decay nostra carne undead. Poenitentiam violent zom biehig hway agite RE:dead pœnitentiam! Vivens mortua sunt apud nos night of the living dead.`,
+      category: "Local Authorities",
+      input_type: "long text",
+      helper_text:
+        "Please be as specific as possible, including figures for time or financial savings"
+    },
+    {
+      question: "Product screenshots",
+      answer:
+        "http://digitalspyuk.cdnds.net/17/15/768x384/landscape-1491824829-hodor-game-of-thrones-hbo.jpg",
+      category: "Images and Demos",
+      input_type: "file upload",
+      helper_text: "Please upload images of your product"
     }
-  },
-  impact: {
-    roi_spreadsheet: {
-      file_url: "file.xlsx",
-      input_type: "file_upload"
-    }
-  },
-  images_demos: {
-    video_demo_url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
-    img_urls: ["image1.jpg", "image2.jpg"]
-  }
+  ]
 };
