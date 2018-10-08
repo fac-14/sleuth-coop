c848fb080cf7d14c0077b7e644581dc442101deb
Jessie Beech <jessie@jessiebeech.com>
2018-10-08 13:03:29 +0100

    add profile styles and rendering

---
+++ client/src/components/ProfileComponents/Block.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default class Block extends React.Component {
+  render() {
+    const { type } = this.props;
+    if ( type === "image" ) {
+      return (
+      <div className={this.props.type + " profile-block"}>
+          <h3>{this.props.heading}</h3>
+          <img src={this.props.answer} alt="content"/>
+        </div>
+      )  
+    } else {
+      return (
+        <div className={this.props.type + " profile-block"}>
+          <h3>{this.props.heading}</h3>
+          <p>{this.props.answer}</p>
+        </div>
+      )
+    }
+  }
+
+}
--- client/src/components/ProfileComponents/Content.js
+++ client/src/components/ProfileComponents/Content.js
@@ -1,9 +1,11 @@
 import React from 'react';
-
+import Block from './Block'
 export default class Content extends React.Component {
   render() {
     return (
-      <h1>Hehe</h1>
+      <div>{this.props.answers.map((el, index) => {
+        return <Block key={index} type={el.input_type} heading={el.question} answer={el.answer}/>;
+      })}</div>
     )
   }
 }
