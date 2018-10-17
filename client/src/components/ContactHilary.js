0a90fbb2e99bf294a3c3cf1f1e998470d119a78f
Emma Ogden <ecogden@gmail.com>
2018-10-17 19:13:18 +0100

    more styling of the contact hilary popup Relates #17

--- client/src/components/ContactHilary.js
+++ client/src/components/ContactHilary.js
@@ -21,58 +21,58 @@ export default class ContactHilary extends React.Component {
           innovation platform, we can offer in-depth advice and assistance on
           building your proposition.
         </p>
+        <div id="sleuth-info">
+          <div className="contact-section">
+            <h2>Contact</h2>
+            <ul>
+              <li>
+                <h4>Email:</h4>
+                <p>hello@sleuth.coop</p>
+              </li>
 
-        <div className="contact-section">
-          <h2>Contact</h2>
-          <ul>
-            <li>
-              <h3>Email:</h3>
-              <p>hello@sleuth.coop</p>
-            </li>
+              <li>
+                <h4>Address:</h4>
+                <p>
+                  Sleuth Cooperative,
+                  <br /> Nye Bevan House, <br /> London, <br /> SW6 7TB, <br />
+                  United Kingdom
+                </p>
+              </li>
+            </ul>
+          </div>
 
-            <li>
-              <h3>Address:</h3>
-              <p>
-                Sleuth Cooperative,
-                <br /> Nye Bevan House, <br /> London, <br /> SW6 7TB, <br />
-                United Kingdom
-              </p>
-            </li>
-          </ul>
-        </div>
+          <div className="sleuth-link-section">
+            <h2>Find us online</h2>
 
-        <div className="contact-section">
-          <h2>Find us online</h2>
-
-          <span className="site-span">
-            <h3>Website:</h3>
-            <p>www.sleuth.coop</p>
-          </span>
-
-          <div className="sleuth-profile-links">
-            <a
-              href="https://www.linkedin.com/in/hilary-simpson-02395418/?ppe=1"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img src={linkedInIcon} alt="linked in" />
+            <a href="www.sleuth.coop" target="_blank" rel="noopener noreferrer">
+              www.sleuth.coop
             </a>
 
-            <a
-              href="https://twitter.com/simpson_hilary?lang=en"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img src={twitterIcon} alt="twitter" />
-            </a>
+            <div className="sleuth-profile-links">
+              <a
+                href="https://www.linkedin.com/in/hilary-simpson-02395418/?ppe=1"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={linkedInIcon} alt="linked in" />
+              </a>
 
-            <a
-              href="https://medium.com/@hilary.simpson"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img src={mediumIcon} alt="medium" />
-            </a>
+              <a
+                href="https://twitter.com/simpson_hilary?lang=en"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={twitterIcon} alt="twitter" />
+              </a>
+
+              <a
+                href="https://medium.com/@hilary.simpson"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={mediumIcon} alt="medium" />
+              </a>
+            </div>
           </div>
         </div>
       </div>
