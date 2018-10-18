b1631ade11f66f772580c619658cfb98f25412e1
Jessie Beech <jessie@jessiebeech.com>
2018-10-18 17:09:21 +0100

    change logo-url name in folder and db, tidy server file Relates #71

--- src/uploadFiles.js
+++ src/uploadFiles.js
@@ -1,13 +1,13 @@
 exports.post = (req, res) => {
     const files = req.files;
-
+    const compId = req.headers.referer.split("profile/")[1].split("/")[0];
     const fileArr = [];
     for (const key in files) {
         fileArr.push(files[key]);
     }
 
     fileArr.forEach(file => {
-        file.mv(`${__dirname}/public/${file.name}`, err => {
+        file.mv(`${__dirname}/public/${compId}-${file.name}`, err => {
             if (err) {
                 return res.status(500).send(err);
             }
