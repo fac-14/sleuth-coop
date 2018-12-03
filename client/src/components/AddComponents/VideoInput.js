7665d6726295ea3eda86233ebedef5d891a3aa5c
Sangita Sunuwar <sangitasunuwar@gmail.com>
2018-12-03 14:30:15 +0000

    fix link/url text display>relates to #138

--- client/src/components/AddComponents/VideoInput.js
+++ client/src/components/AddComponents/VideoInput.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default class VideoInput extends React.Component {
   render() {
-    const { content } = this.props;
+    const { content, answers } = this.props;
     return (
       <fieldset className="video-add">
         <label htmlFor={content.id}>
@@ -16,6 +16,9 @@ export default class VideoInput extends React.Component {
           type="text"
           pattern="(?:https?:\/\/)?(?:youtu\.be\/|(?:www\.|m\.)?youtube\.com\/(?:watch|v|embed)(?:\.php)?(?:\?.*v=|\/))([a-zA-Z0-9\-_]+)"
         />
+        <p>
+          Current Link: {answers[content.id] ? answers[content.id] : "None"}
+        </p>
       </fieldset>
     );
   }
