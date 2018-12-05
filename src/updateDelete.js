e357480705bf2675b9907178e010c012d969b96c
Ben Newman <benjamin-newman@live.co.uk>
2018-12-05 15:58:14 +0000

    fixed the deactivate toggle > #121

--- src/updateDelete.js
+++ src/updateDelete.js
@@ -2,13 +2,7 @@ const updateDelete = require("./database/queries/updateDelete");
 
 exports.post = (req, res) => {
   updateDelete(req.body.compId, req.body.status)
-    .then(() =>
-      res.send(
-        `account status of compId ${req.body.compId} changes to ${
-          req.body.status
-        }`
-      )
-    )
+    .then(() => res.send(req.body.status))
     .catch(err => {
       console.log(err);
       res.send(`Error updating the company account status: ${err}`);
